b8672c2cbafcd9869a1f23c2516d212bdef5fafb
elisabetros <elberoest@gmail.com>
2020-05-26 15:30:33 +0200

    bodyparser

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 
 const cors = require("cors");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(cors());
 
